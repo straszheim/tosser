ded929890966946ce525314e6905fabfb4318990
Troy Straszheim <tstraszheim@x.team>
2017-06-08 15:58:28 -0700

    woot

---
+++ pfft.cpp
@@ -0,0 +1,11 @@
+#include <pybind11/pybind11.h>
+#include <iostream>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pfft, m) {
+
+  m.doc() = "pfft build plugin";
+  m.def("funcy", []() { return std::string("this is funcy"); }, "funcy function");
+
+}
